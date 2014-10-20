f602dd2685e14d28ed40619917c4112d75c112ce
Brad Schneider <brad.schneider@me.com>
2014-10-20 01:05:23 -0500

    Initial commit. Add counter working and counter index showing all counters and add button.

---
+++ app/controllers/simple_controller.rb
@@ -0,0 +1,10 @@
+class SimpleController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def view
+  end
+end
---
+++ test/controllers/simple_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class SimpleControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
+
+  test "should get view" do
+    get :view
+    assert_response :success
+  end
+
+end
